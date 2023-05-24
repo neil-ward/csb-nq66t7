593eab78e352f2eb10ea3893c1a277b343b8d4ff
Code Sandbox <hello@codesandbox.io>
2023-05-24 20:07:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.64ca7b7910186ca2d32516fc9c85b277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7cab8d4727ee8ead34f06df021f6cd",
+    "url": "https://neil-ward.github.io/csb-nq66t7/index.html"
+  },
+  {
+    "revision": "2627929afc3e66d303e5",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/css/main.5e1d73c1.chunk.css"
+  },
+  {
+    "revision": "7b74955090990100ef08",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/js/2.850f02e6.chunk.js"
+  },
+  {
+    "revision": "55d2529880086d75c9e3aeb1360db402",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/js/2.850f02e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2627929afc3e66d303e5",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/js/main.a37edc97.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/js/main.a37edc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cece2525d6be108d2283",
+    "url": "https://neil-ward.github.io/csb-nq66t7/static/js/runtime-main.2949a6a1.js"
+  }
+]);
